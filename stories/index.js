02472975ea718726c426dc6c96d063169f499677
Josh Duck <github@joshduck.com>
2017-03-13 11:10:35 +1000

    Add storybook, prettier and babel

---
+++ stories/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import { storiesOf, action, linkTo } from "@kadira/storybook";
+import DatePicker from "../src";
+
+storiesOf("Date Picker", module).add("with text", () => <DatePicker />);
